7325196f2dd2b241128498ae158ddbfc64aafa84
AlexeyRakitin <amrakitin@gmail.com>
2020-04-07 13:17:00 +0300

    order, order saving for db

--- shop/orderapp/admin.py
+++ shop/orderapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from orderapp.models import Order, OrderItems
+
+
 # Register your models here.
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItems
+    raw_id_fields = ['product']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+                    'address', 'city', 'postal_code', 'created', 'updated',
+                    'paid']
+    list_filter = ['created', 'updated', 'paid']
+    inlines = [OrderItemsInline]
+
+
+admin.site.register(Order, OrderAdmin)
--- shop/orderapp/views.py
+++ shop/orderapp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from .models import OrderItems
+from .forms import OrderCreateForm
+from basketapp.basket import Cart
+
+
 # Create your views here.
+
+
+def order_create(request):
+    """
+    cart - current cart
+    GET - creating form
+    POST - check valid and create order
+    :param request:
+    :return:
+    """
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItems.objects.create(order=order,
+                                          product=item['product'],
+                                          price=item['price'],
+                                          quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orderapp/order_created.html', {'order': order})
+
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orderapp/order_create.html', {'cart': cart, 'form': form})
