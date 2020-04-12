fbe78b3d680b26674d172f17a927d7b25f33a03c
AlexeyRakitin <amrakitin@gmail.com>
2020-04-12 13:18:57 +0300

    categories and detail view

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -79,12 +79,25 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'NAME': 'django_project_db',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'USER': 'django',
+            'PASSWORD': 'Loh4ohquahno',
+            'HOST': 'localhost'
+        }
+    }
+
+
 
 
 # Password validation
@@ -138,3 +151,6 @@ CART_SESSION_ID = 'basket'
 # send mail into console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
+
+# db
+
